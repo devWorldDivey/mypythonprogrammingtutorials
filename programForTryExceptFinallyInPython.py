178be55a2ba3a7ca6f81f71ae7d4db1427d5855f
Divey anand <csediveyanand@gmail.com>
2022-10-25 14:23:36 +0530

    Initial Commit

--- programForTryExceptFinallyInPython.py
+++ programForTryExceptFinallyInPython.py
@@ -2,7 +2,7 @@
 number1 = int(input("Enter a number"))
 number2 = int(input("Enter another number"))
 try:
-    print("When First number is divided by second we get ", (number1//number2))
+    print("When First number is divided by second we get ", (number1 // number2))
 except Exception as e:
     print(e)
 finally:
