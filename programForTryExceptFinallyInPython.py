d00194ca333cc90167fac124ce3e1ca0182789ef
Divey anand <csediveyanand@gmail.com>
2022-10-25 14:22:14 +0530

    Initial Commit

--- programForTryExceptFinallyInPython.py
+++ programForTryExceptFinallyInPython.py
@@ -1,1 +1,9 @@
 """ Program for try except finally in python """
+number1 = int(input("Enter a number"))
+number2 = int(input("Enter another number"))
+try:
+    print("When First number is divided by second we get ", (number1//number2))
+except Exception as e:
+    print(e)
+finally:
+    print("Opps, Sorry I can't divide !!!!!")
