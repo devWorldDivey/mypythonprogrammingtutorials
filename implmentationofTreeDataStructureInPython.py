414f3c37fabe425932c6cdfb49467d28018eb228
Divey anand <csediveyanand@gmail.com>
2022-10-28 14:45:52 +0530

    Initial Commit

--- implmentationofTreeDataStructureInPython.py
+++ implmentationofTreeDataStructureInPython.py
@@ -8,3 +8,32 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while(True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+my_tree.insert(5)
+
+print("Root:", my_tree.root.value)
+print("Root->Left:",my_tree.root.left.value)
+print("Root->Right:",my_tree.root.right.right.value)
