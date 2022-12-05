0e992bd8a12a17b4e599fd5654884eb8f979e04b
Divey anand <csediveyanand@gmail.com>
2022-12-05 16:55:39 +0530

    Initial Commit

---
+++ LeetCode Problems/Easy Problems - Leetcode/876. Middle of the Linked List.py
@@ -0,0 +1,50 @@
+"""
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+"""
+
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def middle(self):
+        pass
+
+
+
+mylinklist = LinkedList(2)
+mylinklist.append(5)
+mylinklist.append(15)
+mylinklist.append(25)
+mylinklist.append(35)
+print(mylinklist.head.value)
+print(mylinklist.tail.value)
+print(mylinklist.length)
