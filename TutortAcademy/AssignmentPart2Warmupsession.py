58577ab2c5f25b4370b5b91a01a6c8fdbc79d90a
Divey Anand <csediveyanand@gmail.com>
2023-03-01 09:02:27 +0530

    Initial Commit

---
+++ TutortAcademy/AssignmentPart2Warmupsession.py
@@ -0,0 +1,54 @@
+"""1672. Richest Customer Wealth"""
+class Solution:
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        res = []
+        for i in accounts:
+            res.append(sum(i))
+
+        return max(res)
+
+    def runningSum(self, nums: list[int]) -> list[int]:
+        res = []
+        for i in range(len(nums)):
+            try:
+                if i == 0:
+                    res.append(nums[i])
+                else:
+                    res.append(nums[i]+res[i-1])
+            except:
+                print("Error")
+        return res
+
+
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        j = list(jewels)
+        counter = 0
+        for i in jewels:
+            if i in list(stones):
+                counter += list(stones).count(i)
+        return counter
+
+    def minimumAbsDifference(self, arr: list[int]) -> list[list[int]]:
+        arr.sort()
+        mindiff = abs(arr[1]-arr[0])
+        for j in range(len(arr)-1):
+            mindiff = min(mindiff,abs(arr[j+1]-arr[j]))
+        res = []
+        for i in range(len(arr)-1):
+            if abs(arr[i+1]-arr[i]) == mindiff:
+                res.append([arr[i],arr[i+1]])
+        return res
+
+    def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+        for i in range(len(arr)-2):
+            if (arr[i] % 2 != 0) and (arr[i+1] % 2 != 0) and (arr[i+2] % 2 != 0):
+                return True
+
+
+
+mysol = Solution()
+mysol.maximumWealth(accounts = [[1,2,3],[3,2,1]])
+mysol.runningSum(nums = [1,2,3,4])
+mysol.numJewelsInStones(jewels = "aA", stones = "aAAbbbb")
+mysol.minimumAbsDifference(arr = [4,2,1,3])
+mysol.threeConsecutiveOdds(arr = [1,2,34,3,4,5,7,23,12])
