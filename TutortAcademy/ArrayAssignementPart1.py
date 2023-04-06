b384c04a65bbb252f2f3649b74a7f8ed1042cb8f
Divey Anand <csediveyanand@gmail.com>
2023-04-06 12:52:51 +0530

    6th April 2023 V01

--- TutortAcademy/ArrayAssignementPart1.py
+++ TutortAcademy/ArrayAssignementPart1.py
@@ -24,11 +24,29 @@ class Solution:
                 j -= 1
 
 #3. https://leetcode.com/problems/merge-sorted-array/
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    # """
+    # Do not return anything, modify nums1 in-place instead.
+    # """"
+        j = 0
+        k = nums2[j]
+        for i in range(len(nums1)):
+            if nums1[i] >= k:
+                nums1.pop()
+                nums1.insert(i,k)
+                j += 1
+                k=nums2[j]
+                print(k)
+        print(nums1)
+      
+    
+
 
 
 
 
 # Object and method calling
 mysol = Solution()
-mysol.twoSum([2,7,11,15], target = 9)
-print(mysol.twoSum1([2,7,11,15], target = 9))
+# mysol.twoSum([2,7,11,15], target = 9)
+# mysol.twoSum1([2,7,11,15], target = 9)
+mysol.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
