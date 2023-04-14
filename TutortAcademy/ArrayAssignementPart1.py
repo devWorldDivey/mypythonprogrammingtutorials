7862288bc27f373fe392b638c4a3e550b14384e3
Divey Anand <csediveyanand@gmail.com>
2023-04-14 20:26:30 +0530

    1

--- TutortAcademy/ArrayAssignementPart1.py
+++ TutortAcademy/ArrayAssignementPart1.py
@@ -28,19 +28,81 @@ class Solution:
     # """
     # Do not return anything, modify nums1 in-place instead.
     # """"
-        j = 0
-        k = nums2[j]
-        for i in range(len(nums1)):
-            if nums1[i] >= k:
-                nums1.pop()
-                nums1.insert(i,k)
-                j += 1
-                k=nums2[j]
-                print(k)
-        print(nums1)
-      
+    #Solution using 2 pointers
+        #last index nums1
+        last = m+n-1
+        #merge in reverse order
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m-=1
+            else:
+                nums1[last] = nums2[n-1]
+                n-=1
+            last -=1
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n,last=n-1,last-1
+            
+#4. https://leetcode.com/problems/pascals-triangle/
+    def generate(self, numRows: int) -> list[list[int]]:
+        res=[[1]]
+        
+        for i in range(numRows - 1):
+            temp =[0]+res[-1]+[0]
+            row=[]
+            for j in range(len(res[-1])+1):
+                row.append(temp[j]+temp[j+1])
+            res.append(row)
+        
+        return res
     
+#5. https://leetcode.com/problems/pascals-triangle-ii/
+    def getRow(self, rowIndex: int) -> list[int]:
+        res=[[1]]      
+        for i in range(rowIndex):
+            temp =[0]+res[-1]+[0]
+            row=[]
+            for j in range(len(res[-1])+1):
+                row.append(temp[j]+temp[j+1])
+            res.append(row)
+        print(res[rowIndex])
+        return res
+    
+#6. https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    def maxProfit(self, prices: list[int]) -> int:
+        buy = prices[0]
+        maxprofit = 0
+        sell = 0
+        for i in prices:
+            if i < buy:
+                buy = i
+            else:
+                sell = i
+                maxprofit = max(maxprofit,sell-buy)
+        print(maxprofit)
+        
+#7. https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+    def maxProfit2(self, prices: list[int]) -> int:
+        profit = 0
+        buy=prices[0]
+        sell=0
+        share = 1
+        for i in prices:
+            if i < buy:
+                buy = i
+            else:
+                sell = i
+                if (sell - buy > 0) and share == 1:
+                    profit += sell-buy
+                    share -= 1
+        print(profit)
+        
 
+                
+
+
+        
 
 
 
@@ -49,4 +111,9 @@ class Solution:
 mysol = Solution()
 # mysol.twoSum([2,7,11,15], target = 9)
 # mysol.twoSum1([2,7,11,15], target = 9)
-mysol.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
+# mysol.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
+# mysol.generate(numRows = 5 )
+# mysol.getRow(rowIndex=0)
+# mysol.maxProfit(prices = [7,1,5,3,6,4])
+mysol.maxProfit2(prices = [7,1,5,3,6,4])
+
